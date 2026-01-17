7beacca3ecade10f3648788fc17fa71b416bf7a0
pasumaibharatam <pasumaibharatam@gmail.com>
2026-01-17 15:25:46 +0530

    bck

--- auth.py
+++ auth.py
@@ -6,9 +6,9 @@ MAX_BCRYPT_LENGTH = 72  # bcrypt limit
 
 def hash_password(password: str) -> str:
     # truncate to 72 bytes
-    password_bytes = password.encode("utf-8")[:MAX_BCRYPT_LENGTH]
+    password_bytes = password.encode("utf-8")[:72]
     return pwd_context.hash(password_bytes)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    plain_bytes = plain_password.encode("utf-8")[:MAX_BCRYPT_LENGTH]
+    plain_bytes = plain_password.encode("utf-8")[:72]
     return pwd_context.verify(plain_bytes, hashed_password)
